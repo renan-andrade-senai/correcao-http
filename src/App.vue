143291593222a0d6f6f346d54cfe5bc0ada8065c
renan-andrade-senai <renan.andrade@edu.sc.senai.br>
2026-05-10 17:46:02 -0300

    desafios correcao

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,48 @@
 <script setup lang="ts">
-import { onMounted, ref } from 'vue';
+import { computed, onMounted, ref } from 'vue';
 import Produto from './models/Produto';
 import http from './http';
+import { AxiosError } from 'axios';
 
 const produto = ref({} as Produto)
-
 const lista = ref([] as Produto[])
+const filtro = ref('')
+const loading = ref(false)
 
 async function carregaProdutos() {
-  const response = await http.get('/produtos')
-  lista.value = response.data
+  try {
+    loading.value = true
+    const response = await http.get('/produtos')
+    lista.value = response.data
+  } catch (error) {
+    alert('Erro ao carregar produtos')
+    console.log((<AxiosError>error).response?.data)
+  } finally {
+    loading.value = false
+  }
 }
 
 async function cadastraProduto() {
-  await http.post('/produtos', produto.value)
-  alert('Produto cadastrado com sucesso')
-  produto.value = {} as Produto
+  try {
+    await http.post('/produtos', produto.value)
+    alert('Produto cadastrado com sucesso')
+    produto.value = {} as Produto
+    await carregaProdutos()
+  } catch (error) {
+    alert('Erro ao cadastrar produto')
+    console.log((<AxiosError>error).response?.data)
+  }
+}
+
+async function excluirProduto(p: Produto) {
+  await http.delete(`/produtos/${p.id}`)
+  alert(`Produto ${p.nome} excluído com sucesso`)
   await carregaProdutos()
 }
 
 onMounted(carregaProdutos)
+
+const listaFiltrada = computed(() => lista.value.filter(p => !filtro.value || p.nome.includes(filtro.value)))
 
 </script>
 
@@ -40,7 +63,7 @@ onMounted(carregaProdutos)
             <label class="form-label">Quantidade</label>
             <input v-model="produto.quantidade" type="number" class="form-control">
           </div>
-          <button type="submit" class="btn btn-primary">
+          <button type="submit" class="btn btn-primary mb-3">
             Salvar
           </button>
         </form>
@@ -48,21 +71,33 @@ onMounted(carregaProdutos)
     </div>
     <div class="row">
       <div class="col">
-        <table class="table">
+        <div class="mb-3">
+          <input type="text" placeholder="Filtro" v-model="filtro" class="form-control">
+        </div>
+        <div class="spinner-border text-success" role="status" v-show="loading">
+          <span class="visually-hidden">Loading...</span>
+        </div>
+        <table class="table" v-show="!loading">
           <thead>
             <tr>
               <th>Id</th>
               <th>Nome</th>
               <th>Preço</th>
               <th>Quantidade</th>
+              <th>Ações</th>
             </tr>
           </thead>
           <tbody>
-            <tr v-for="p in lista" :key="p.id">
+            <tr v-for="p in listaFiltrada" :key="p.id">
               <td>{{ p.id }}</td>
               <td>{{ p.nome }}</td>
               <td>R$ {{ p.preco }}</td>
               <td>{{ p.quantidade }}</td>
+              <td>
+                <button @click="excluirProduto(p)" class="btn btn-danger">
+                  Excluir
+                </button>
+              </td>
             </tr>
           </tbody>
         </table>
--- src/http/index.ts
+++ src/http/index.ts
@@ -1,7 +1,10 @@
 import axios from "axios";
 
 const http = axios.create({
-    baseURL: 'http://localhost:8080'
+    baseURL: 'http://localhost:8080',
+    headers: {
+        'Authorization': 'Bearer exemplo'
+    }
 })
 
 export default http
