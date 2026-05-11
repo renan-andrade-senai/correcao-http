b623963d5b1ab1eecf4fc6ebdfb31c1c77451bde
renan-andrade-senai <renan.andrade@edu.sc.senai.br>
2026-05-11 16:15:47 -0300

    pre correcao

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,4 @@
 <script setup lang="ts">
-import { onMounted, ref } from 'vue';
-import type Produto from './models/Produto';
-import http from './http';
-
-const produto = ref({} as Produto)
-
-const lista = ref([] as Produto[])
-
-async function carregaProdutos() {
-  const response = await http.get('/produtos')
-  lista.value = response.data
-}
-
-async function cadastraProduto() {
-  await http.post('/produtos', produto.value)
-  alert('Produto cadastrado com sucesso')
-  produto.value = {} as Produto
-  await carregaProdutos()
-}
-
-onMounted(carregaProdutos)
 
 </script>
 
@@ -27,18 +6,18 @@ onMounted(carregaProdutos)
   <div class="container">
     <div class="row">
       <div class="col">
-        <form @submit.prevent="cadastraProduto">
+        <form>
           <div class="mb-3">
             <label class="form-label">Nome</label>
-            <input v-model="produto.nome" type="text" class="form-control">
+            <input type="text" class="form-control">
           </div>
           <div class="mb-3">
             <label class="form-label">Preço</label>
-            <input v-model="produto.preco" type="number" class="form-control">
+            <input type="number" class="form-control">
           </div>
           <div class="mb-3">
             <label class="form-label">Quantidade</label>
-            <input v-model="produto.quantidade" type="number" class="form-control">
+            <input type="number" class="form-control">
           </div>
           <button type="submit" class="btn btn-primary">
             Salvar
@@ -58,11 +37,11 @@ onMounted(carregaProdutos)
             </tr>
           </thead>
           <tbody>
-            <tr v-for="p in lista" :key="p.id">
-              <td>{{ p.id }}</td>
-              <td>{{ p.nome }}</td>
-              <td>R$ {{ p.preco }}</td>
-              <td>{{ p.quantidade }}</td>
+            <tr>
+              <td></td>
+              <td></td>
+              <td>R$ </td>
+              <td></td>
             </tr>
           </tbody>
         </table>
--- src/models/Produto.ts
+++
@@ -1,6 +0,0 @@
-export default interface Produto {
-    id: number,
-    nome: string,
-    preco: number,
-    quantidade: number
-}
